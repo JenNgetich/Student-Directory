0c4c5c50c1c67a27a86e53b5e8f79f1d452a46d6
Jennifer Ngetich <JenNgetich@users.noreply.github.com>
2025-07-15 12:03:14 -0400

    Update table-view.js

--- views/table-view.js
+++ views/table-view.js
@@ -68,58 +68,58 @@ TableView = Backbone.View.extend({
 //Manually Add the Student models to the class collection
 aprClass.add([
    {
-  first: 'Andrew',
-  last: 'McIntee',
-  github: 'andrwm',
-  email: 'andrew@email.com',
+  first: 'A',
+  last: 'M',
+  github: 'example1',
+  email: 'example1@email.com',
   photo: 'https://secure.gravatar.com/avatar/3f628a01946bb72e992e4460b90a0f8b?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   },
 
   {
-  first: 'Lindsay',
-  last: 'McDougall',
-  github: 'codesprouter',
-  email: 'Lindsay@email.com',
+  first: 'L',
+  last: 'M',
+  github: 'example2',
+  email: 'example2@email.com',
   photo: 'https://secure.gravatar.com/avatar/c0c7bde88d97510751d17072978703da?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   },
 
   {
-  first: 'Rasheeda',
-  last: 'Mickel',
-  github: 'gvldesigner',
-  email: 'gvldesigner@hotmail.com',
+  first: 'R',
+  last: 'M',
+  github: 'example3',
+  email: 'example3@hotmail.com',
   photo: 'http://glogp.gratitudelog.com/assets/users/11675/large/Obama_Rsm.jpg?1260118063'
   },
 
 
   {
-  first: 'Enrique',
-  last: 'Acevedo',
-  github: 'ek1770',
-  email: 'enrique.acevedo@gmail.com',
+  first: 'E',
+  last: 'A',
+  github: 'example4',
+  email: 'example4@gmail.com',
   photo: 'https://secure.gravatar.com/avatar/be253da919fce9659ca693b904ecd096?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   },
 
   {
-  first: 'Tyler',
-  last: 'Sloan',
-  email: 'tyler.g.sloan@gmail.com',
-  github: 'tylersloan',
+  first: 'T',
+  last: 'S',
+  email: 'example5@gmail.com',
+  github: 'example5',
   photo: 'https://secure.gravatar.com/avatar/5f62f7b74ac94241581499c0361d21c4?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   },
 
   {
-  first: 'Daniel',
-  last: 'Jeffords',
-  email: 'daniel@email.com',
-  github: 'djsra',
+  first: 'D',
+  last: 'J',
+  email: 'example6@email.com',
+  github: 'example6',
   photo: 'https://secure.gravatar.com/avatar/da8a5a98804afce7c9598230ac702868?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   },
   
-  {first: 'Jen',
-  last: 'Ngetich',
-  email: 'jennifer.ngetich@gmail.com',
-  github: 'jcoden2',
+  {first: 'J',
+  last: 'N',
+  email: 'example7@email.com',
+  github: 'example7',
   photo: 'https://secure.gravatar.com/avatar/5096a67e7d9a7aa6246c2f24807c2ea4?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png'
   }
 ])
